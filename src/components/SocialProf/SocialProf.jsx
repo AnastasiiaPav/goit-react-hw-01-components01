5f9b0a0881806cdbfc5007523f8ee3eec357c848
Anastasiia <pavlishinka12@icloud.com>
2022-11-16 15:53:21 +0200

    ..

---
+++ src/components/SocialProf/SocialProf.jsx
@@ -0,0 +1,42 @@
+import propTypes from 'prop-types'
+
+const User = ({ username, tag, location, avatar, followers, views, likes }) => {
+    return (
+        <div class="profile">
+            <div class="description">
+                <img
+                    src={avatar}
+                    alt="User avatar"
+                    class="avatar"
+                />
+                <p class={username}></p>
+                <p class={tag}></p>
+                <p class={location}></p>
+            </div>
+
+            <ul class="stats">
+                <li>
+                    <span class={followers}>Followers 👯‍♀️</span>
+                    <span class="quantity"></span>
+                </li>
+                <li>
+                    <span class={views}>Views 👁‍🗨</span>
+                    <span class="quantity"></span>
+                </li>
+                <li>
+                    <span class={likes}>Likes 🖤</span>
+                    <span class="quantity"></span>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+User.propTypes = {
+    username: propTypes.string,
+    avatar: propTypes.string,
+    location: propTypes.string,
+    followers: propTypes.number,
+    views: propTypes.number,
+    likes: propTypes.number,
+}
